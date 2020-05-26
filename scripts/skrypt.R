ebd287d3b106b1aa6a39eb667898a85c52030d68
Keionn <zdeb.katarzyna@gmail.com>
2020-05-27 00:24:33 +0200

    Korpus dokumentow -wstepne przetwarzanie

--- scripts/skrypt.R
+++ scripts/skrypt.R
@@ -4,16 +4,89 @@ library (hunspell)
 library (stringr)
 
 #zmiana katalogu roboczego 
-workDir <- "C:\\Users\\Beniamin\\Desktop\\Projekty06 Projekt\\Projekty06"
+workDir <- "G:\\R-Project06\\Projekty06"
 setwd(workDir)
 
 
-#definicja katalog?w projektu
+#definicja katalogow projektu
 inputDir <- ".\\data"
 outputDir <- ".\\results"
 scriptsDir <- ".\\scripts"
 workspaceDir <- ".\\workspaces"
 
-#utworzenie katalogu wyj?ciowego
+
 dir.create(outputDir, showWarnings = FALSE)
 dir.create(workspaceDir, showWarnings = FALSE)
+
+#utworzenie korpusu dokumentow
+corpusDir <- paste(
+  inputDir, 
+  "Literatura - oryginal",
+  sep = "\\"
+)
+
+corpus <- VCorpus(
+  DirSource(
+    corpusDir,
+    pattern = "*.txt",
+    encoding = "UTF-8"
+  ),
+  readerControl = list(
+    language = "pl_PL"
+  )
+)
+
+#wstepne przetwarzanie: usuniêcie z podzia³u na akapity
+pasteParagraphs <- content_transformer(function(x,char) paste(x, collapse = char))
+corpus <- tm_map(corpus, pasteParagraphs, " ")
+
+#wstepne przetwarzanie: stoplista
+corpus <- tm_map(corpus, removeNumbers)
+corpus <- tm_map(corpus, removePunctuation)
+corpus <- tm_map(corpus, content_transformer(tolower))
+stoplistFile <- paste(
+  inputDir, 
+  "stopwords_pl.txt",
+  sep = "\\"
+)
+
+stoplist <- readLines(stoplistFile, encoding = "UTF-8")
+corpus <- tm_map(corpus, removeWords, stoplist)
+corpus <- tm_map(corpus, stripWhitespace)
+
+#wstepne przetwarzanie: usuniecie em dash i 3/4
+removeChar <- content_transformer(function(x,pattern) gsub(pattern, "", x))
+corpus <- tm_map(corpus, removeChar, intToUtf8(8722))
+corpus <- tm_map(corpus, removeChar, intToUtf8(190))
+
+
+#wstepne przetwarzanie: lemantyzacja
+polish <- dictionary(lang="pl_PL")
+lemmatize <- function(text) {
+  simpleText <- str_trim(as.character(text))
+  parsedText <- strsplit(simpleText, split = " ")
+  newTextVec <- hunspell_stem(parsedText[[1]], dict = polish)
+  for (i in 1:length(newTextVec)) {
+    if (length(newTextVec[[i]]) == 0) newTextVec[i] <- parsedText[[1]][i]
+    if (length(newTextVec[[i]]) > 1) newTextVec[i] <- newTextVec[[i]][1]
+  }
+  newText <- paste(newTextVec, collapse = " ")
+  return(newText)
+}
+corpus <- tm_map(corpus, content_transformer(lemmatize))
+
+#wstepne przetwarzanie: usuniecie rozszerzen
+cutExtensions <- function(document){
+  meta(document, "id") <- gsub(pattern = "\\.txt$", replacement = "", meta(document, "id"))
+  return(document)
+}
+corpus <- tm_map(corpus, cutExtensions)
+
+#wstepne przetwarzanie: zapisanie przetworzonych plikow do katalogu
+preprocessedDir <- paste(
+  outputDir, 
+  "Literatura - przetworzone",
+  sep = "\\"
+)
+dir.create(preprocessedDir)
+writeCorpus(corpus, path = preprocessedDir)
