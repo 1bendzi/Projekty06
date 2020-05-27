8a3c42991b3d09c62d5327d8ab1b5b502bb7f15e
1bendzi <32649585+1bendzi@users.noreply.github.com>
2020-05-27 14:19:54 +0200

    zrobiony jeden wykres pca

--- scripts/skrypt.R
+++ scripts/skrypt.R
@@ -159,3 +159,64 @@ matrixFile <- paste(
 )
 write.table(tdmTfidfBoundsMatrix, file = matrixFile, sep = ";", dec = ",", col.names = NA)
 
+
+
+#tutaj zaczyna siê analiza glownych skladowych  (PCA)
+
+#analiza g³ównych sk³adowych
+pca <- prcomp(dtmTfidfBounds)
+x <- pca$x[,1]
+y <- pca$x[,2]
+
+#przygotowanie legendy
+legend <- paste(
+  paste("d", 1:length(rownames(dtmTfidfBoundsMatrix)),sep = ""),
+  rownames(dtmTfidfBoundsMatrix),
+  sep = "<-"
+)
+
+#wykres dokumentów w przestrzeni dwuwymiarowej
+plot(
+  x,
+  y,
+  #xlim = c(-0.5,-0.2),
+  #ylim = c(-0.2,0.1),
+  xlab="Wspó³rzêdna syntetyczna 1", 
+  ylab="Wspó³rzêdna syntetyczna 2",
+  main="Analiza g³ównych sk³adowych", 
+  col = "orange"
+)
+text(
+  x, 
+  y, 
+  labels = paste("d", 1:length(rownames(dtmTfidfBoundsMatrix)),sep = ""), 
+  pos = 3,
+  col = "orange"
+)
+legend("bottom", legend, cex=.5, text.col = "orange")
+
+#eksport wykresu do pliku .png
+plotFile <- paste(
+  outputDir,
+  "\\",
+  "pca.png",
+  sep = ""
+)
+png(file = plotFile)
+plot(
+  x,
+  y,
+  xlab="Wspó³rzêdna syntetyczna 1", 
+  ylab="Wspó³rzêdna syntetyczna 2",
+  main="Analiza g³ównych sk³adowych", 
+  col = "orange"
+)
+text(
+  x, 
+  y, 
+  labels = paste("d", 1:length(rownames(dtmTfidfBoundsMatrix)),sep = ""), 
+  pos = 3,
+  col = "orange"
+)
+legend("bottom", legend, cex=.65, text.col = "orange")
+dev.off()
